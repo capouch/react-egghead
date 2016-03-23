cf43260e7f57e6c08b4b54ea32f5b8762163ae7a
Brian Capouch <brianc@palaver.net>
2016-03-23 04:15:59 -0400

    Initial commit at Lesson #5

---
+++ App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// This code imlements "State Basics" No. 5 video Maddalone egghead.io
+// Note this is CLASS-BASED JavaScript
+class App extends React.Component {
+  // Works just like Java--call super() before adding your own code
+  constructor () {
+    super();
+    this.state = {txt: 'This is my state text'}
+  }
+
+  // Used by the form to input the state variable
+  update(e) {
+    this.setState({txt: e.target.value})
+  }
+
+  // Required in all React components; it's what actually puts up the component
+  render() {
+    return (<div>
+    <input type="text"
+      // Watch the field and call update when its contents change
+      onChange={this.update.bind(this)} />
+    // Render the state data in a <h2> element
+    <h2>{this.state.txt}</h2>
+    </div>
+  );
+  }
+}
+// This renders the overall class in its container div
+ReactDOM.render (
+  <App />,
+  document.getElementById('app')
+)
+
+// ES2015 syntax; see companion import in main.js
+export default App
